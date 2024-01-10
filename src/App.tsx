d45ff09817a34de30a5f83483c1f2597ad1f676f
Ahmed Osman <ahmedaosman00@gmail.com>
2024-01-10 10:43:15 -0800

    Add react-router-dom package and update routing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import AuthLayout from "./_auth/AuthLayout";
+import SignInForm from "./_auth/Forms/SignInForm";
+import SignUpForm from "./_auth/Forms/SignUpForm";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
 import "./globals.css";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/*public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+
+        {/*private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
