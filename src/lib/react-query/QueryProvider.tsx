1c965aa6ea850f3c5beabc86ac47afcd938a7997
Ahmed Osman <ahmedaosman00@gmail.com>
2024-01-13 12:53:16 -0800

    Delete unused files and components

--- src/lib/react-query/QueryProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { ReactNode } from "react";
-
-const queryClient = new QueryClient();
-export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
-};
