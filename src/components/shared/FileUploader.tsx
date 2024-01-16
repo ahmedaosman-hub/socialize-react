99a71ecf7da5792c889ac6da9faab69aecbd6bef
Ahmed Osman <ahmedaosman00@gmail.com>
2024-01-16 10:07:44 -0800

    Refactor EditPost component and PostForm component

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -11,7 +11,7 @@ type FileUploaderProps = {
 
 const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
   const [file, setFile] = useState<File[]>([]);
-  const [fileUrl, setFileUrl] = useState<string>(mediaUrl);
+  const [fileUrl, setFileUrl] = useState(mediaUrl);
 
   const onDrop = useCallback(
     (acceptedFiles: FileWithPath[]) => {
